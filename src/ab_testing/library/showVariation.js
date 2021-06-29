147d030abbbee46e9e78b9fac2075a423f25fcc1
Khalil Najjar <khn@alteos.com>
2021-06-29 15:46:18 +0200

    finish styling

--- src/ab_testing/library/showVariation.js
+++ src/ab_testing/library/showVariation.js
@@ -4,11 +4,4 @@ export function showVariation(variationNodes) {
   variationNodes[sessionClient.getVariation()].forEach((node) => {
     node.style.display = null;
   });
-
-  const sheet = document.styleSheets[0];
-  for (const i in sheet.cssRules) {
-    if (sheet.cssRules[i].selectorText === '[data-variation]') {
-      sheet.deleteRule(i);
-    }
-  }
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './styles.css';
+import './styles.scss';
 import { mountAbTesting } from './ab_testing';
 
 mountAbTesting();
